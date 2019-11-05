b1b5220a2b3e71ceefa113878808763cdc8d91f4
Charlie Broom <broomc425@gmail.com>
2019-11-05 20:48:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -15,7 +15,8 @@ def introduction_with_language_optional(name, language = "ruby")
   puts "Hi, my name is #{name} and I am learning to program in Ruby."
 end
 
-def introduction_with_language_optional(name, language, language = "")
+def introduction_with_language_optional(name, language, language = "Python")
+  puts 
 
 
 
